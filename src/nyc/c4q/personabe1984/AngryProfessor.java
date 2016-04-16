6e0772378c0a6135a457a27b8f279e31ef4cae63
personabe1984 <personabe1984@gmail.com>
2016-04-15 20:33:00 -0400

    trying out angry prof, works for some test cases but not all

--- src/nyc/c4q/personabe1984/AngryProfessor.java
+++ src/nyc/c4q/personabe1984/AngryProfessor.java
@@ -6,24 +6,45 @@ public class AngryProfessor {
 
     public static void main(String[] args) {
         int studentPresent = 0;
+        int[] studentPresentArray;
+        int[] threshold;
+        int index = 0;
 
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        if(!(t >= 1 && t <=10) ) System.exit(0);
+        if(!(t >= 1 && t <= 10))System.exit(0);
+        studentPresentArray = new int[t];
+        threshold = new int[t];
 
         for(int a0 = 0; a0 < t; a0++){
             int n = in.nextInt();
-            //if(n <=)System.exit(0);
+            if(!(n >= 1 && n <= 100))System.exit(0);
 
             int k = in.nextInt();
+            if(!(k >= 1 && k <= n))System.exit(0);
+
             int a[] = new int[n];
             for(int a_i=0; a_i < n; a_i++){
                 a[a_i] = in.nextInt();
+                if(!(a[a_i] >= -100 && a[a_i] <= 100))System.exit(0);
+                if(a[a_i] <= 0){
+                    studentPresent++;
+                }
             }
+            studentPresentArray[index] = studentPresent;
+            threshold[index] = k;
+            index++;
+
+            studentPresent = 0;
         }
+
+        for(int i = 0; i < t; i++){
+            theClassIsCancelled(studentPresentArray[i],threshold[i]);
+        }
+
     }
 
-    public void theClassIsCancelled(int studentPresent, int k){
+    public static void theClassIsCancelled(int studentPresent, int k){
         if(studentPresent < k){
             System.out.println("YES");
         }else{
